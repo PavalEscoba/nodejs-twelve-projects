44c5e7ac592acab1f72f91fd8369c7af7f96893b
Paval Miatlitski <paval.escoba@gmail.com>
2019-04-11 21:57:37 +0300

    feat: first project is done

--- simpleserver/server.js
+++ simpleserver/server.js
@@ -1,28 +1,46 @@
-// const http = require('http');
-
-// const hostname = '127.0.0.1';
-// const port = 1337;
-
-// http.createServer((req, res) =>{
-//   res.writeHead(200, {'Content-Type': 'text/plain'});
-//   res.end('Hello World\n');
-// }).listen(port, hostname, () => {
-//   console.log(`Server running at http://${hostname}:${port}/`)
-// });
-
-// OR YOU MAY WRITE THIS WAY
-
 const http = require('http');
+const url = require('url');
+const path = require('path');
+const fs = require('fs');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const mimeTypes = {
+  "html": "text/html",
+  "jpeg": "image/jpeg",
+  "jpg": "image/jpg",
+  "png": "image/png",
+  "js": "text/javascript",
+  "css": "text/css"
+};
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello! I\'m server');
-});
+http.createServer(function (req, res) {
+  var uri = url.parse(req.url).pathname;
+  var fileName = path.join(process.cwd(), unescape(uri));
+  console.log('loading ' + uri);
+  var stats;
+  try {
+    stats = fs.lstatSync(fileName);
+  } catch (error) {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.write('404 Not Found\n');
+    res.end();
+    return;
+  }
+  if (stats.isFile()) {
+    var mimeType = mimeTypes[path.extname(fileName).split('.').reverse()[0]];
+    res.writeHead(200, { 'Content-type': mimeType });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}`);
-});
+    var fileStream = fs.createReadStream(fileName);
+    fileStream.pipe(res);
+  } else if (stats.isDirectory()) {
+    res.writeHead(302, {
+      'Location': 'index.html'
+    });
+    res.end();
+  } else {
+    res.writeHead(500, {
+      'Content-Type': 'text/plain'
+    });
+    res.write('500 Internal Error');
+    res.end();
+  }
+}).listen(1337);
